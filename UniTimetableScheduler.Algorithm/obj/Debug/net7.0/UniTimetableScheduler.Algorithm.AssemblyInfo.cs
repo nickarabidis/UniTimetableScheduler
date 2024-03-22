d4872c665cacc768146573071b9d192f81bce7de
Nick Arabidis <nikosarabidis@gmail.com>
2024-03-22 17:00:44 +0200

    small changes

--- UniTimetableScheduler.Algorithm/obj/Debug/net7.0/UniTimetableScheduler.Algorithm.AssemblyInfo.cs
+++ UniTimetableScheduler.Algorithm/obj/Debug/net7.0/UniTimetableScheduler.Algorithm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniTimetableScheduler.Algorithm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3384969d213fc93f0674ead41f0ac55c43d53eff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4236682ffca334a8399083f6c45e5f6fde8f9333")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniTimetableScheduler.Algorithm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniTimetableScheduler.Algorithm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
